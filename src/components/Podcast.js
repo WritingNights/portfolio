d0b9fc2195f1b5ca80afd2986c520e1bf6ec8fc3
Daniel Jones <danieljones@Daniels-MacBook-Pro-2.local>
2025-04-03 22:39:19 -0700

    Podcast update minor

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,9 +1,19 @@
-import banner from "./data/podcast-data.js";
+import moment from "moment";
+
+import banner from "./Podcast/Banner.png";
+
+import podcastFrame from "./data/podcast-data.js";
 
 export default function Podcast(props) {
+  const source = podcastFrame[moment().date() - 1 % podcastFrame.length].src;
+
+//  const source = "3hKikFMbF9YY5eSx2b1Uex";
+
   return (
     <section id="podcast" className="manager">
-      <img src={banner} alt="banner" style={{width: "100%"}} />
+      <div style={{ backgroundImage: `url(${banner})`}} className="podcastBanner">
+        <iframe style={{margin: "1rem", borderRadius: "12px"}} title="Novelist Journey Chapter 1" src={`https://open.spotify.com/embed/episode/${source}?utm_source=generator`} width="50%" height="342" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </div>
     </section>
   );
 }
--- src/components/data/podcast-data.js
+++ src/components/data/podcast-data.js
@@ -1,3 +1,66 @@
-import banner from "../Podcast/Banner.png";
 
-export default banner;
+
+const podcastFrame = [
+  {
+    episode: 1,
+    src: "3hKikFMbF9YY5eSx2b1Uex"
+  },
+  {
+    episode: 2,
+    src: "0I6IsAED7LjGLKPD3idNGz"
+  },
+  {
+    episode: 3,
+    src: "7ALSLpnatTm1eqiEbpBxBf"
+  },
+  {
+    episode: 4,
+    src: "77A2GtHUFpVT8jqb6KA4Tt"
+  },
+  {
+    episode: 5,
+    src: "57YNzGX0BEN4wo44PDIWK5"
+  },
+  {
+    episode: 6,
+    src: "3hicjNfkdUaQcY4rHPvdeA"
+  },
+  {
+    episode: 7,
+    src: "0XWDAwralN2hxgJL1c2tnE"
+  },
+  {
+    episode: 8,
+    src: "0skywDyPkJfTMJtNGGObwd"
+  },
+  {
+    episode: 9,
+    src: "0YoDcEhuo3Z9YTSFTXE4VP"
+  },
+  {
+    episode: 10,
+    src: "5XwTZgM4x8B4VIsXSbRcmJ"
+  },
+  {
+    episode: 11,
+    src: "2P2a2xdmHzr1TPWfMOSxXw"
+  },
+  {
+    episode: 12,
+    src: "3q12ewErvSMuqzSdsMsNNK"
+  },
+  {
+    episode: 13,
+    src: "65zR94VcXtHJH6G2G3WVCM"
+  },
+  {
+    episode: 14,
+    src: "0gHdQPZEIaP09AShZWnWY3"
+  },
+  {
+    episode: 15,
+    src: "0i6U2YLEhzJKFdcBERuXck"
+  }
+]
+
+export default podcastFrame;
